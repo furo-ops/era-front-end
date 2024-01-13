dc0019bf1c9933e61af16fa153839fa98ee53545
furo-ops <fernando.rufo97@gmail.com>
2024-01-13 19:02:28 +0100

    Second client's request

--- script.js
+++ script.js
@@ -32,3 +32,42 @@ imagesLoaded(images, { background: true }, function() {
   // All images are loaded; now you can start your animations
   showDemo();
 }).on('progress', updateProgress);
+
+
+////COUNTDOWN
+function updateTimer() {
+  future  = Date.parse("January 25, 2024 20:00:00");
+  now     = new Date();
+  diff    = future - now;
+
+  days  = Math.floor( diff / (1000*60*60*24) );
+  hours = Math.floor( diff / (1000*60*60) );
+  mins  = Math.floor( diff / (1000*60) );
+  secs  = Math.floor( diff / 1000 );
+
+  d = days;
+  h = hours - days  * 24;
+  m = mins  - hours * 60;
+  s = secs  - mins  * 60;
+
+  document.getElementById("timer")
+    .innerHTML =
+      '<div>' + d + '<span>days</span></div>' +
+      '<div>' + h + '<span>hours</span></div>' +
+      '<div>' + m + '<span>minutes</span></div>' +
+      '<div>' + s + '<span>seconds</span></div>' ;
+}
+setInterval('updateTimer()', 1000 );
+
+
+// NAVIGATION AND UL LI SCROLL SMOOTH 
+// Optional: JavaScript for smooth scrolling (if browser support is insufficient)
+document.querySelectorAll('.side-navigation a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
